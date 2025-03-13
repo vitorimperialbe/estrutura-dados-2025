b9cbcbda47a8326efff286eeba101ce3645e5724
Marco Mazzei <marco.mazzei@fatec.sp.gov.br>
2025-03-12 23:29:21 -0300

    Inclusão de modelos

---
+++ 20250313/modelos/EscopoMetodoEParametro.java
@@ -0,0 +1,27 @@
+public class EscopoMetodoEParametro {
+     // Escopo de classe 
+     static int x = 11;
+  
+     // Escopo de instância - variável de instância
+     private int y = 33;
+ 
+     // Escopo de parâmetro - (x)
+     public void testaFuncao(int x) {
+         // Escopo de método (t) só existe durante a execução do método
+         EscopoMetodoEParametro t = new EscopoMetodoEParametro(); 
+         this.x = 22; 
+         y = 44; 
+ 
+         // Printing variables with different scopes
+         System.out.println("EscopoMetodoEParametro.x: " + EscopoMetodoEParametro.x); 
+         System.out.println("t.x: " + t.x); 
+         System.out.println("t.y: " + t.y); 
+         System.out.println("y: " + y); 
+     }
+ 
+     // Main Method
+     public static void main(String args[]) {
+         Geeks t = new Geeks();
+         t.testFunc(5); 
+     }
+}
