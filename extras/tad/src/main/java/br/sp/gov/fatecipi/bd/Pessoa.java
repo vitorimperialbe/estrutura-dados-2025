d6490ced734bdc45fc8344b6b2fbc5b021c763f8
Marco Mazzei <marco.mazzei@fatec.sp.gov.br>
2025-08-18 16:41:23 -0300

    Implementação inserção Arvone Binaria - Método Recursivo.

--- extras/tad/src/main/java/br/sp/gov/fatecipi/bd/Pessoa.java
+++ extras/tad/src/main/java/br/sp/gov/fatecipi/bd/Pessoa.java
@@ -1,9 +1,5 @@
 package br.sp.gov.fatecipi.bd;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
 public class Pessoa {
     private int idPessoa;
     private String nome;
@@ -37,32 +33,6 @@ public class Pessoa {
 
     public void setCpf(String cpf) {
         this.cpf = cpf;
-    }
-
-    public void inserir() {
-        String sql_str = "INSERT INTO tb_pessoa(nome_pessoa, cpf_pessoa) VALUES (?,?)";
-        ConnectionFactory connectionFactory = new ConnectionFactory();
-        try (Connection c = connectionFactory.obtemConnection()){
-            PreparedStatement ps = c.prepareStatement(sql_str);
-            ps.setString(1,this.getNome());
-            ps.setString(2,this.getCpf());
-            ps.execute();
-        } catch (Exception e) {
-            System.out.println(e.getStackTrace());
-        }
-    }
-
-    public void atualizar() {
-        String sql = "UPDATE tb_pessoa SET nome_pessoa = ?, cpf_pessoa = ? WHERE id_pessoa = ?";
-        ConnectionFactory connectionFactory = new ConnectionFactory();
-        try (Connection con = connectionFactory.obtemConnection()){
-            PreparedStatement ps = con.prepareStatement(sql);
-            ps.setString(1,this.getNome());
-            ps.setString(2,this.getCpf());
-            ps.setInt(3,this.getIdPessoa());
-        } catch(SQLException se) {
-            System.out.println(se.getStackTrace());
-        }
     }
 
     
